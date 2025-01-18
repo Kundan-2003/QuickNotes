14ffe4268a54ba8305465ae9993b7515cbb45034
Kundan <128500021+Kundan-2003@users.noreply.github.com>
2025-01-18 15:45:49 +0530

    Update ApiEndPoint.js

--- Frontend/services/ApiEndPoint.js
+++ Frontend/services/ApiEndPoint.js
@@ -1,16 +1,17 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const instance =axios.create({
-    baseURL:'http://localhost:4001',
-    headers:{
-        'Content-Type':'application/json'
+const instance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4001', // Use environment variable for flexibility
+    headers: {
+        'Content-Type': 'application/json',
     },
-    withCredentials:true
-})
+    withCredentials: true, // Ensure credentials like cookies are sent with requests
+});
 
-export const get=(url,params)=>instance.get(url,{params})
+export const get = (url, params) => instance.get(url, { params });
 
-export const post=(url,data)=>instance.post(url,data)
+export const post = (url, data) => instance.post(url, data);
 
-export const put=(url,data)=>instance.put(url,data)
-export const dele=(url)=>instance.delete(url)
+export const put = (url, data) => instance.put(url, data);
+
+export const dele = (url) => instance.delete(url);
